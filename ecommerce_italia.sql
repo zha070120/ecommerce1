53f011e997356f5c8e00225ca92595a0ee005b45
zhouhongan1-netizen <zhouhongan1@gmail.com>
2026-05-09 22:51:33 +0200

    db

--- ecommerce_italia.sql
+++ ecommerce_italia.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Creato il: Mag 06, 2026 alle 13:05
--- Versione del server: 10.4.32-MariaDB
--- Versione PHP: 8.2.12
+-- 主机： 127.0.0.1
+-- 生成日期： 2026-05-09 22:49:32
+-- 服务器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ecommerce_italia`
+-- 数据库： `ecommerce_italia`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `carello`
+-- 表的结构 `carello`
 --
 
 CREATE TABLE `carello` (
@@ -33,7 +33,7 @@ CREATE TABLE `carello` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabella carrelli clienti';
 
 --
--- Dump dei dati per la tabella `carello`
+-- 转存表中的数据 `carello`
 --
 
 INSERT INTO `carello` (`id_carello`, `email`) VALUES
@@ -42,7 +42,7 @@ INSERT INTO `carello` (`id_carello`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `cliente`
+-- 表的结构 `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -55,7 +55,7 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabella anagrafica clienti';
 
 --
--- Dump dei dati per la tabella `cliente`
+-- 转存表中的数据 `cliente`
 --
 
 INSERT INTO `cliente` (`email`, `nome`, `cognome`, `indirizzo`, `cap`, `password`) VALUES
@@ -64,27 +64,30 @@ INSERT INTO `cliente` (`email`, `nome`, `cognome`, `indirizzo`, `cap`, `password
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `ordine`
+-- 表的结构 `ordine`
 --
 
 CREATE TABLE `ordine` (
   `id_ordine` int(11) NOT NULL COMMENT 'ID univoco ordine',
   `email` varchar(100) NOT NULL COMMENT 'Email cliente (chiave esterna)',
   `data_ordine` date NOT NULL COMMENT 'Data di creazione ordine',
-  `stato_ordine` varchar(10) NOT NULL COMMENT 'Stato ordine (es: attivo, consegnato, annullato)'
+  `stato_ordine` enum('attivo','spedito','consegnato','annullato') NOT NULL DEFAULT 'attivo' COMMENT 'Stato ordine (es: attivo, consegnato, annullato)'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabella testata ordini';
 
 --
--- Dump dei dati per la tabella `ordine`
+-- 转存表中的数据 `ordine`
 --
 
 INSERT INTO `ordine` (`id_ordine`, `email`, `data_ordine`, `stato_ordine`) VALUES
-(1, 'aaa@gmail.com', '2026-05-06', 'attivo');
+(1, 'aaa@gmail.com', '2026-05-06', 'attivo'),
+(2, 'aaa@gmail.com', '2026-05-07', 'annullato'),
+(3, 'aaa@gmail.com', '2026-05-07', 'attivo'),
+(4, 'aaa@gmail.com', '2026-05-07', 'consegnato');
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `prodotto`
+-- 表的结构 `prodotto`
 --
 
 CREATE TABLE `prodotto` (
@@ -97,19 +100,20 @@ CREATE TABLE `prodotto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabella catalogo prodotti';
 
 --
--- Dump dei dati per la tabella `prodotto`
+-- 转存表中的数据 `prodotto`
 --
 
 INSERT INTO `prodotto` (`id_prodotto`, `nome`, `prezzo`, `p_iva`, `quantita_disponibile`, `indirizzo_img`) VALUES
 (1, 'Smartphone X1', 299.99, '0123456789012', 50, 'https://picsum.photos/300/300?random=1'),
-(2, 'Laptop Pro 15', 899.90, '0123456789012', 19, 'https://picsum.photos/300/300?random=2'),
+(2, 'Laptop Pro 15', 899.90, '0123456789012', 14, 'https://picsum.photos/300/300?random=2'),
 (3, 'Maglia Cotone Uomo', 29.90, '0123456789012', 100, 'https://picsum.photos/300/300?random=3'),
-(4, 'Jeans Slim Donna', 49.99, '0123456789012', 75, 'https://picsum.photos/300/300?random=4');
+(4, 'Jeans Slim Donna', 49.99, '0123456789012', 75, 'https://picsum.photos/300/300?random=4'),
+(5, 'matita', 2.00, '88888888', 995, 'https://picsum.photos/300/300?random=5');
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `p_c`
+-- 表的结构 `p_c`
 --
 
 CREATE TABLE `p_c` (
@@ -118,17 +122,10 @@ CREATE TABLE `p_c` (
   `pezzi` int(11) NOT NULL COMMENT 'Numero di pezzi nel carrello'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Dettaglio righe carrello';
 
---
--- Dump dei dati per la tabella `p_c`
---
-
-INSERT INTO `p_c` (`id_prodotto`, `id_carello`, `pezzi`) VALUES
-(3, 1, 5);
-
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `p_o`
+-- 表的结构 `p_o`
 --
 
 CREATE TABLE `p_o` (
@@ -140,16 +137,19 @@ CREATE TABLE `p_o` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Dettaglio righe ordine';
 
 --
--- Dump dei dati per la tabella `p_o`
+-- 转存表中的数据 `p_o`
 --
 
 INSERT INTO `p_o` (`id_prodotto`, `id_ordine`, `pezzi`, `prezzo_singolo`, `prezzo_tot`) VALUES
-(2, 1, 1, 899.90, 899.90);
+(2, 1, 1, 899.90, 899.90),
+(2, 3, 5, 899.90, 4499.50),
+(3, 2, 5, 29.90, 149.50),
+(5, 4, 5, 2.00, 10.00);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `venditore`
+-- 表的结构 `venditore`
 --
 
 CREATE TABLE `venditore` (
@@ -161,116 +161,117 @@ CREATE TABLE `venditore` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Tabella anagrafica venditori';
 
 --
--- Dump dei dati per la tabella `venditore`
+-- 转存表中的数据 `venditore`
 --
 
 INSERT INTO `venditore` (`p_iva`, `ragione_sociale`, `indirizzo`, `cap`, `password`) VALUES
-('0123456789012', 'Elettronica SRL', 'Via Roma 123, Milano', '20100', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
+('0123456789012', 'Elettronica SRL', 'Via Roma 123, Milano', '20100', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'),
+('88888888', 'shabi', 'via aaaaaaaaaaaaaaaaa', '33333', '$2y$10$dPr4Lfgrf2BKtD8we/JhdelYGd/6U45fQgU1dVmAdCPPIFRzBx/pO');
 
 --
--- Indici per le tabelle scaricate
+-- 转储表的索引
 --
 
 --
--- Indici per le tabelle `carello`
+-- 表的索引 `carello`
 --
 ALTER TABLE `carello`
   ADD PRIMARY KEY (`id_carello`),
   ADD KEY `email` (`email`);
 
 --
--- Indici per le tabelle `cliente`
+-- 表的索引 `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`email`);
 
 --
--- Indici per le tabelle `ordine`
+-- 表的索引 `ordine`
 --
 ALTER TABLE `ordine`
   ADD PRIMARY KEY (`id_ordine`),
   ADD KEY `email` (`email`);
 
 --
--- Indici per le tabelle `prodotto`
+-- 表的索引 `prodotto`
 --
 ALTER TABLE `prodotto`
   ADD PRIMARY KEY (`id_prodotto`),
   ADD KEY `p_iva` (`p_iva`);
 
 --
--- Indici per le tabelle `p_c`
+-- 表的索引 `p_c`
 --
 ALTER TABLE `p_c`
   ADD PRIMARY KEY (`id_prodotto`,`id_carello`) COMMENT 'Chiave primaria composta',
   ADD KEY `id_carello` (`id_carello`);
 
 --
--- Indici per le tabelle `p_o`
+-- 表的索引 `p_o`
 --
 ALTER TABLE `p_o`
   ADD PRIMARY KEY (`id_prodotto`,`id_ordine`) COMMENT 'Chiave primaria composta',
   ADD KEY `id_ordine` (`id_ordine`);
 
 --
--- Indici per le tabelle `venditore`
+-- 表的索引 `venditore`
 --
 ALTER TABLE `venditore`
   ADD PRIMARY KEY (`p_iva`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT per la tabella `carello`
+-- 使用表AUTO_INCREMENT `carello`
 --
 ALTER TABLE `carello`
   MODIFY `id_carello` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco carrello', AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT per la tabella `ordine`
+-- 使用表AUTO_INCREMENT `ordine`
 --
 ALTER TABLE `ordine`
-  MODIFY `id_ordine` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco ordine', AUTO_INCREMENT=2;
+  MODIFY `id_ordine` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco ordine', AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT per la tabella `prodotto`
+-- 使用表AUTO_INCREMENT `prodotto`
 --
 ALTER TABLE `prodotto`
-  MODIFY `id_prodotto` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco prodotto', AUTO_INCREMENT=5;
+  MODIFY `id_prodotto` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID univoco prodotto', AUTO_INCREMENT=6;
 
 --
--- Limiti per le tabelle scaricate
+-- 限制导出的表
 --
 
 --
--- Limiti per la tabella `carello`
+-- 限制表 `carello`
 --
 ALTER TABLE `carello`
   ADD CONSTRAINT `carello_ibfk_1` FOREIGN KEY (`email`) REFERENCES `cliente` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Limiti per la tabella `ordine`
+-- 限制表 `ordine`
 --
 ALTER TABLE `ordine`
   ADD CONSTRAINT `ordine_ibfk_1` FOREIGN KEY (`email`) REFERENCES `cliente` (`email`) ON UPDATE CASCADE;
 
 --
--- Limiti per la tabella `prodotto`
+-- 限制表 `prodotto`
 --
 ALTER TABLE `prodotto`
   ADD CONSTRAINT `prodotto_ibfk_1` FOREIGN KEY (`p_iva`) REFERENCES `venditore` (`p_iva`) ON UPDATE CASCADE;
 
 --
--- Limiti per la tabella `p_c`
+-- 限制表 `p_c`
 --
 ALTER TABLE `p_c`
   ADD CONSTRAINT `p_c_ibfk_1` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotto` (`id_prodotto`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `p_c_ibfk_2` FOREIGN KEY (`id_carello`) REFERENCES `carello` (`id_carello`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Limiti per la tabella `p_o`
+-- 限制表 `p_o`
 --
 ALTER TABLE `p_o`
   ADD CONSTRAINT `p_o_ibfk_1` FOREIGN KEY (`id_prodotto`) REFERENCES `prodotto` (`id_prodotto`) ON UPDATE CASCADE,
